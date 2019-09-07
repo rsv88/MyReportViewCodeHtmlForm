6e5de24a7a3afc28a724c81e9ac8df4c7a722c6c
Ronald Sanchez <sanchez88elite@outlook.com>
2019-09-07 11:48:23 -0600

    Agregar archivos de proyecto.

---
+++ FrmReportVIewHelp.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MyReportViewCodeHtmlForm
+{
+    partial class FrmReportVIewHelp
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.ReportViewHtml = new System.Windows.Forms.WebBrowser();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnPrint);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(712, 51);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Option";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(98, 20);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(16, 19);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // ReportViewHtml
+            // 
+            this.ReportViewHtml.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReportViewHtml.Location = new System.Drawing.Point(3, 60);
+            this.ReportViewHtml.MinimumSize = new System.Drawing.Size(20, 20);
+            this.ReportViewHtml.Name = "ReportViewHtml";
+            this.ReportViewHtml.Size = new System.Drawing.Size(712, 327);
+            this.ReportViewHtml.TabIndex = 0;
+            // 
+            // FrmReportVIewHelp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ReportViewHtml);
+            this.Name = "FrmReportVIewHelp";
+            this.Size = new System.Drawing.Size(718, 387);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.WebBrowser ReportViewHtml;
+    }
+}
---
+++ FrmReportVIewHelp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyReportViewCodeHtmlForm
+{
+    public partial class FrmReportVIewHelp : UserControl
+    {
+        public FrmReportVIewHelp()
+        {
+            InitializeComponent();
+        }
+
+        private ReportPlantilla plantilla;
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+        /// <summary>
+        /// Generar la vista para el reporte con el codigo html
+        /// </summary>
+        /// <param name="codeHtml">Code HTML</param>
+        public void SetContentHtmlView()
+        {
+            plantilla = new ReportPlantilla();
+            plantilla.CodeHtml = GetHtml;
+            ReportViewHtml.DocumentText = plantilla.ReportView;
+            ReportViewHtml.Refresh();
+        }
+        private string GetHtml { get; set; }
+        /// <summary>
+        /// Agregar el codigo html al control
+        /// </summary>
+        /// <param name="codeHtml"></param>
+        public void SetCodeHTML(StringBuilder codeHtml)
+        {
+            GetHtml = codeHtml.ToString();
+        }
+        /// <summary>
+        /// Imprimir contenido
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            ReportViewHtml.ShowPrintPreviewDialog();
+        }
+        /// <summary>
+        /// Guardar el conetenido en html para imprimir
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+           
+            ReportViewHtml.ShowSaveAsDialog();
+        }
+    }
+}
